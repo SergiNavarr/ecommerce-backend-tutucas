57c051957de3f44dcdbd3d9b09b24ce7aced252e
Sergio Navarro <nachonavarro197@gmail.com>
2025-01-17 16:29:30 -0300

    cloudinary add

--- config/plugins.js
+++ config/plugins.js
@@ -1,13 +1,26 @@
-module.exports = {
-
-'strapi-neon-tech-db-branches': {
+module.exports = ({ env }) => ({
+  "strapi-neon-tech-db-branches": {
     enabled: true,
     config: {
-      neonApiKey: "a9g5h61sunyyky5kccnp8gxu7fb1noskeyq6p21vrxqdfn28mqvm51whj7192l8g", // get it from here: https://console.neon.tech/app/settings/api-keys
+      neonApiKey:
+        "a9g5h61sunyyky5kccnp8gxu7fb1noskeyq6p21vrxqdfn28mqvm51whj7192l8g", // get it from here: https://console.neon.tech/app/settings/api-keys
       neonProjectName: "ecommerce-tutucas", // the neon project under wich your DB runs
       neonRole: "neondb_owner", // create it manually under roles for your project first
-      gitBranch: "main" // branch can be pinned via this config option. Will not use branch from git then. Usefull for preview/production deployment
-    }
-},
-
-}
+      gitBranch: "main", // branch can be pinned via this config option. Will not use branch from git then. Usefull for preview/production deployment
+    },
+  },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+});
